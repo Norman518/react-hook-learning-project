979f487ed85104b2903ef7adfe69cc1e1596fd82
Vincent T <46739285+Norman518@users.noreply.github.com>
2020-01-14 12:28:18 -0500

    ingredients are added to ingredient list

--- src/components/Ingredients/IngredientForm.js
+++ src/components/Ingredients/IngredientForm.js
@@ -8,7 +8,7 @@ const IngredientForm = React.memo(props => {
   const [enteredAmount, setAmount] = useState('');
   const submitHandler = event => {
     event.preventDefault();
-    // ...
+    props.onAddIngredient({ title: enteredTitle, amount: enteredAmount });
   };
 
   return (
--- src/components/Ingredients/IngredientList.js
+++ src/components/Ingredients/IngredientList.js
@@ -7,10 +7,10 @@ const IngredientList = props => {
     <section className="ingredient-list">
       <h2>Loaded Ingredients</h2>
       <ul>
-        {props.ingredients.map(ig => (
-          <li key={ig.id} onClick={props.onRemoveItem.bind(this, ig.id)}>
-            <span>{ig.title}</span>
-            <span>{ig.amount}x</span>
+        {props.ingredients.map(ingredient => (
+          <li key={ingredient.id} onClick={props.onRemoveIngredient.bind(this, ingredient.id)}>
+            <span>{ingredient.amount}x</span>
+            <span>{ingredient.title}</span>
           </li>
         ))}
       </ul>
