75b4e8d9c03ea6c38639df6e1c858b6abeafe4b5
Vincent T <46739285+Norman518@users.noreply.github.com>
2020-01-17 09:55:04 -0500

    ingredients are fetched from firebase

--- src/components/Ingredients/Ingredients.js
+++ src/components/Ingredients/Ingredients.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 import IngredientList from './IngredientList';
 import IngredientForm from './IngredientForm';
@@ -6,6 +6,18 @@ import Search from './Search';
 
 const Ingredients = () => {
   const [ingredients, setIngredients] = useState([]);
+
+  useEffect(() => {
+    fetch('https://react-hooks-update-3e000.firebaseio.com/ingredients.json')
+      .then(response => response.json())
+      .then(data => {
+        const loadedIngredients = [];
+        for (const key in data) {
+          loadedIngredients.push({ id: key, title: data[key].title, amount: data[key].amount });
+        }
+        setIngredients(loadedIngredients);
+      });
+  }, []);
 
   const addIngredientHandler = ingredient => {
     fetch('https://react-hooks-update-3e000.firebaseio.com/ingredients.json', {
