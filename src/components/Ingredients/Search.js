38abbc29d2cb19b0f2896f319957a8ec4f4432f7
Vincent T <46739285+Norman518@users.noreply.github.com>
2020-01-19 11:03:23 -0500

    filter doesnt run on every keystroke but at pauses

--- src/components/Ingredients/Search.js
+++ src/components/Ingredients/Search.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 
 import Card from '../UI/Card';
 import './Search.css';
@@ -6,19 +6,27 @@ import './Search.css';
 const Search = React.memo(props => {
   const { onLoadIngredients } = props;
   const [filter, setFilter] = useState('');
+  const inputRef = useRef();
 
   useEffect(() => {
-    const query = filter.length === 0 ? '' : `?orderBy="title"&equalTo="${filter}"`;
-    fetch(`https://react-hooks-update-3e000.firebaseio.com/ingredients.json${query}`)
-      .then(response => response.json())
-      .then(data => {
-        const loadedIngredients = [];
-        for (const key in data) {
-          loadedIngredients.push({ id: key, title: data[key].title, amount: data[key].amount });
-        }
-        onLoadIngredients(loadedIngredients);
-      });
-  }, [filter, onLoadIngredients]);
+    const timer = setTimeout(() => {
+      if (filter === inputRef.current.value) {
+        const query = filter.length === 0 ? '' : `?orderBy="title"&equalTo="${filter}"`;
+        fetch(`https://react-hooks-update-3e000.firebaseio.com/ingredients.json${query}`)
+          .then(response => response.json())
+          .then(data => {
+            const loadedIngredients = [];
+            for (const key in data) {
+              loadedIngredients.push({ id: key, title: data[key].title, amount: data[key].amount });
+            }
+            onLoadIngredients(loadedIngredients);
+          });
+      }
+    }, 500);
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [filter, onLoadIngredients, inputRef]);
 
   return (
     <section className="search">
@@ -26,6 +34,7 @@ const Search = React.memo(props => {
         <div className="search-input">
           <label>Filter by Title</label>
           <input
+            ref={inputRef}
             type="text"
             value={filter}
             onChange={event => {
